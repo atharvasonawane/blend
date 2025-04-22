66e0a7e3d49fce3c16b2feed193ec4f4ba36e09a
Atharva Sonawane <Atharva Sonawane>
2025-04-22 20:53:45 +0530

    fix:Tab Navigation working fine

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,33 +4,68 @@ import {Tabs} from "expo-router";
 import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 
-const TabIcon = () => {
+const TabIcon = ({focused,icon,title}: any) => {
+
+    if(focused) {
+        return (
+
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 justify-center items-center rounded-full overflow-hidden mt-3"
+            >
+                <Image source={icon} tintColor="black" className="size-5"/>
+
+                <Text className="ml-2 font-semibold">{title}</Text>
+
+            </ImageBackground>
+        )
+    }
+
     return(
-
-        <ImageBackground
-            source={images.highlight}
-            className="flex flex-row w-full flex-1 min-w-[112] min-h-14 justify-center items-center rounded-full overflow-hidden "
-        >
-            <Image source={icons.home} tintColor="#151312" className="size-5"  />
-
-            <Text className="ml-2 font-semibold">Home</Text>
-
-        </ImageBackground>
+        <View className="size-full justify-center items-center rounded-full mt-4">
+            <Image source={icon} tintColor="white" className="size-5"/>
+        </View>
     )
 }
 
 const _Layout = () => {
   return (
 
-      <Tabs>
+      <Tabs
+      screenOptions={{
+          tabBarShowLabel: false,
+          tabBarItemStyle:{
+              width:'100%',
+              height:'100%',
+              justifyContent:'center',
+              alignItems:'center'
+          },
+          tabBarStyle:{
+              backgroundColor:'#0f0D23',
+              borderRadius:50,
+              marginHorizontal:20,
+              marginBottom:36,
+              height:52,
+              overflow:'hidden',
+              position:'absolute',
+              // borderWidth:1,
+              borderColor:'#0f0D23'
+
+
+
+          }
+      }}>
           <Tabs.Screen
               name="index"
               options={{
                   title: 'Home',
                   headerShown: false,
                   tabBarIcon: ({focused}) => (
-
-                      <TabIcon/>
+                      <TabIcon
+                          focused={focused}
+                          icon={icons.home}
+                          title="Home"
+                      />
                   )
               }}/>
 
@@ -40,7 +75,11 @@ const _Layout = () => {
                   title: 'Search',
                   headerShown: false,
                   tabBarIcon: ({focused}) => (
-                      <TabIcon/>
+                      <TabIcon
+                          focused={focused}
+                          icon={icons.search}
+                          title="Search"
+                      />
                   )
               }}/>
 
@@ -50,7 +89,11 @@ const _Layout = () => {
                   title: 'Profile',
                   headerShown: false,
                   tabBarIcon: ({focused}) => (
-                      <TabIcon/>
+                      <TabIcon
+                      focused={focused}
+                      icon={icons.person}
+                      title="Profile"
+                      />
                   )
               }}/>
 
@@ -60,7 +103,11 @@ const _Layout = () => {
                   title: 'Saved',
                   headerShown: false,
                   tabBarIcon: ({focused}) => (
-                      <TabIcon/>
+                      <TabIcon
+                          focused={focused}
+                          icon={icons.save}
+                          title="Saved"
+                      />
                   )
               }}/>
 
